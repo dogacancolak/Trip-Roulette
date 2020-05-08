b654c6063fac88d973ad8f5c3a0ca18a06b35ac5
erceozmet <55060224+erceozmet@users.noreply.github.com>
2020-05-08 16:14:44 +0300

    al

--- main.py
+++ main.py
@@ -1,11 +1,55 @@
-from  kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import Screen
+from kivymd.app import MDApp
+from kivymd.theming import ThemableBehavior
+from kivymd.uix.list import OneLineIconListItem, MDList
+from kivymd.uix import MDAdaptiveWidget
 from kivymd.theming import ThemeManager
 
-class MainApp(App):
-    theme_cls = ThemeManager()
-    
+class ContentNavigationDrawer(BoxLayout):
+    pass
 
-MainApp().run()
+class ItemDrawer(OneLineIconListItem):
+    icon = StringProperty()
 
 
+class DrawerList(ThemableBehavior, MDList):
+    def set_color_item(self, instance_item):
+        """Called when tap on a menu item."""
 
+        # Set the color of the icon and text for the menu item.
+        for item in self.children:
+            if item.text_color == self.theme_cls.text_color:
+                item.text_color = self.theme_cls.text_color
+                break
+        instance_item.text_color = self.theme_cls.text_color
+
+class HomePage(FloatLayout):
+     pass
+
+class TripRouletteApp(MDApp): 
+
+    def build(self):
+        self.theme_cls.primary_palette = 'Green'
+        self.theme_cls.theme_style = "Light"  # "Light"
+       
+        
+    def on_start(self):
+        icons_item = {
+            "folder": "Account Details",
+            "account-multiple": "Preferences",
+            "star": "Starred",
+            "history": "Recent",
+            "checkbox-marked": "Shared with me",
+            "upload": "Upload",
+        }
+        for icon_name in icons_item.keys():
+            self.root.ids.content_drawer.ids.md_list.add_widget(
+                ItemDrawer(icon=icon_name, text=icons_item[icon_name])
+            )
+
+
+TripRouletteApp().run()
