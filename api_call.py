1d9acb3d037e37be916fab7e720081914751599d
erceozmet <55060224+erceozmet@users.noreply.github.com>
2020-05-15 23:04:06 +0300

    KOCAMAN_YARRAK

--- api_call.py
+++ api_call.py
@@ -2,7 +2,7 @@ import urllib.request
 import json
 import time
 
-valid_location_types = ["airport", "library", \
+valid_location_types = {"airport", "library", \
                         "amusement_park", "aquarium",\
                         "liquor_store", "art_gallery",\
                         "atm", "bakery", "lodging", "bar", \
@@ -12,31 +12,24 @@ valid_location_types = ["airport", "library", \
                         "campground", "painter", "park", \
                         "parking", "car_wash", "pharmacy", \
                         "casino", "church", \
-                        "restaurant",\
-                        "spa", "florist",\
-                        "stadium", "store", "gym", "tourist_attraction", "university", "zoo"\
-                        ]
-# ISTANBUL
-# radius  = 10000
-# lat = 41.059995
-# lon = 28.987315
+                        "restaurant", "spa", "florist",\
+                        "stadium", "store", "gym", "tourist_attraction", \
+                        "university", "zoo" \
+                        }
 
-def get_places_in_radius(user_info):
+def get_places_in_radius(user_info, place_types):
     lat       = user_info.lat
     lon       = user_info.lon
     radius    = user_info.radius
     max_price = user_info.budget
-    interests = user_info.interests
-    food      = user_info.food
     
     key            = 'AIzaSyA4H5RbPwYejTlXVI1hjio_4q4XYS_Ubts'
     endpoint       = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
     location       = str(lat) + ',' + str(lon)
-    place_types    = food + interests
     
     requests = []
-    tokens = []
-    results = []
+    tokens   = []
+    results  = []
     for place in place_types:
         if place in valid_location_types:
             place_search_word = 'type'      # to be inserted in the API request
@@ -57,17 +50,10 @@ def get_places_in_radius(user_info):
             requests.append(endpoint + nav_request)
             results.append(response["results"])
 
-    # for p in results:
-    #     print("\nResult AMK")
-    #     for res in p:
-    #         print("Name: ", res["name"])
-
-    # print(len(requests))
     tokens_left = True
     while tokens_left:
         tokens_left = False
         i = 0 
-        # print(min(len(tokens), len(requests), len(results)))
         while i < min(len(tokens), len(requests), len(results)):
             if tokens[i] != 0:
                 tokens_left = True
@@ -77,7 +63,6 @@ def get_places_in_radius(user_info):
                 new_response = json.loads(urllib.request.urlopen(new_request).read())
 
                 if new_response["status"] == 'OK':
-                    # print('amjik')
                     results[i].extend(new_response["results"])
                     if 'next_page_token' in new_response:
                         tokens[i] = new_response["next_page_token"]
@@ -88,22 +73,22 @@ def get_places_in_radius(user_info):
     return results
 
 
-class Amcik():
-    pass
+# class Amcik():
+#     pass
 
-amcik = Amcik()
-amcik.lat = 42.406722   # Tufts location as default (easter egg lol)
-amcik.lon = -71.116469
-amcik.radius = 10000
-amcik.interests = ['attraction', 'museum', 'shopping_mall', 'monument', 'hiking']
-amcik.food = ['restaurant', 'bar', 'cafe']
-amcik.trip_length = 4
-amcik.budget = 4
-amcik.transportation = 'walking'
+# amcik = Amcik()
+# amcik.lat = 42.406722   # Tufts location as default (easter egg lol)
+# amcik.lon = -71.116469
+# amcik.radius = 10000
+# amcik.interests = ['attraction', 'museum', 'shopping_mall', 'monument', 'hiking']
+# amcik.food = ['restaurant', 'bar', 'cafe']
+# amcik.trip_length = 4
+# amcik.budget = 4
+# amcik.transportation = 'walking'
 
-results = get_places_in_radius(amcik)
+# results = get_places_in_radius(amcik, amcik.interests)
 
-for p in results:
-    print("\nResult AMK")
-    for res in p:
-        print("Name: ", res["name"])
+# for p in results:
+#     print("\nResult AMK")
+#     for res in p:
+#         print("Name: ", res["name"])
