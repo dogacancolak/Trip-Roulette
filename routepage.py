d592e216c36e6bc9ab415661dca7ee1452733d0e
erceozmet <55060224+erceozmet@users.noreply.github.com>
2020-05-16 13:48:11 +0300

    12

--- routepage.py
+++ routepage.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
 
+
 from api_call import get_places_in_radius
 import timeit
 
@@ -14,21 +15,4 @@ class RoutePage(Screen):
         interest_places = get_places_in_radius(user_info, user_info.interests)
         food_places     = get_places_in_radius(user_info, user_info.food)
         
-        # stop = timeit.default_timer()
-        # for p in interest_places:
-        #     print("\nResult AMK")
-        #     for res in p:
-        #         print("Name: ", res["name"])
-        
-        # print("yemeks memeks")
-        
-        # for p in food_places:
-        #     print("\nResult AMK")
-        #     for res in p:
-        #         print("Name: ", res["name"])        
-
-        # print('Total Time: ', stop - start) 
-      
-
-# 4.5 + 6.4 + 5.4 
 
