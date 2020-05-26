42ebde36ef5e9d1d32fc0f8d2ad6ac3bd87eae1c
dcolak01 <54953926+dcolak01@users.noreply.github.com>
2020-05-26 19:42:45 +0300

    hes

--- routepage.py
+++ routepage.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen
 
@@ -9,28 +10,56 @@ from kivy.garden.mapview import MapMarker
 import trip
 import concurrent.futures
 import time
+import webbrowser
+import threading
 
 class Waypoint(MapMarker):
     pass
 
 class RoutePage(Screen):
     map = ObjectProperty(None)
+    waypoints = None
+    url = None
 
     def show_route_page(self):
+        # threading.Thread(target=self.show_loading_page).start()
+        # trip_details   = trip.generate_trip()
+        # print(trip_details)
 
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
         executor.submit(self.show_loading_page)
-        f2 = executor.submit(trip.generate_trip)
+            
         # f2 = executor.submit(self.test_function)
 
-        def done_callback(future_obj, *args):
+        trip_details = []
+        f2 = executor.submit(trip.generate_trip, trip_details)
+        
+
+            
+        def done_callback(future, *args):
             app = App.get_running_app()
             app.root.windows.current = app.root.routepage.name
             self.map.center_on(app.user_info.lat, app.user_info.lon)
+            print(trip_details)
 
         f2.add_done_callback(done_callback)
-  
-    def show_loading_page(self):
+            
+            # trip_details   = f2.result()
+            # self.waypoints = trip_details[0]
+            # self.url       = trip_details[1]
+
+        # trip_details   = f2.result()
+        # self.waypoints = trip_details[0]
+        # self.url       = trip_details[1]
+
+        # app = App.get_running_app()
+        # app.root.windows.current = app.root.routepage.name
+        # self.map.center_on(app.user_info.lat, app.user_info.lon)
+
+        # webbrowser.open(url)
+
+    def show_loading_page(self, *args):
         app = App.get_running_app()
 
         app.root.homepage.dialog.dismiss()
